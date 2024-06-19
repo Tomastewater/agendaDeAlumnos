b89be084965cff4d75d88739f4423356077eb90d
Tomastewater <98992309+Tomastewater@users.noreply.github.com>
2024-06-19 00:03:40 -0300

    Update Conectar.java
    Conexion actualizada a la actual base de datos

--- conectar/Conectar.java
+++ conectar/Conectar.java
@@ -14,20 +14,25 @@ import javax.swing.JOptionPane;
 
 public class Conectar {
 
-    public static final String URL = "jdbc:mysql://localhost/agenda";     //pruebajava es el nombre de la base de datos
-    public static final String USER = "prueba";
-    public static final String CLAVE = "1234";
+    public static final String URL = "jdbc:mysql://localhost/agendappi2";     //agendappi2 es el nombre de la base de datos
+    public static final String USER = "prueba";       
+    public static final String CLAVE = "";        // No tiene clave
     
+    /**
+     * Funcion que hace conexion a la base de datos, sin esta funcion no puedes acceder a ella.
+     * @return 
+     */
     public Connection getConexion(){
         
-        Connection con = null;    // iniciamos un objeto sin nigun valor usando null, o sea un objeto vacio.
+        Connection con = null;    // iniciamos un objeto sin nigun valor usando null, o sea un objeto vacio, esperando a que
+                                  // si se conecta satisfactoriamente a la base de datos entonces la conexion sera positiva, de otro modo sera denegada
         
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = (Connection) DriverManager.getConnection(URL, USER, CLAVE);
+            con = (Connection) DriverManager.getConnection(URL, USER, CLAVE);   // conexion positiva, se guarda en con
         } catch (Exception e){
             e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "ERROR DE CONEXION", "ERROR", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "ERROR DE CONEXION", "ERROR", JOptionPane.ERROR_MESSAGE);  // alerta si hubo un error de conexion, Revisa el usuario o la clave en tu gestor de base de datos
         }
         return con;
     }
